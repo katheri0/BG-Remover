6440cbf6de85c163bc291b427df1370c6c3e7d8b
katheri0 <singularityqoo.ooq@gmail.com>
2026-01-23 23:38:25 +0300

    style(tkinter )warming up

---
+++ BG-R.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("720x512")
+root.title("BG-Remover")
+label = tk.Label(root, text="BG-Remover ", font=("Arial", 27))
+label.pack(padx=20, pady=10)
+root.mainloop()
